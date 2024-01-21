795278721fd510d928c7aa5cd6279f93266340c0
Chanphirom Sok <65760336+chanphiromsok@users.noreply.github.com>
2024-01-21 15:12:28 +0700

    WIP

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: [
+    "universe",
+    "universe/native",
+    "universe/web",
+    "universe/shared/typescript-analysis",
+  ],
+
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.d.ts"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  ],
+  plugins: ["react-hooks"],
+  rules: {
+    "import/order": "off",
+  },
+  env: {
+    node: true,
+  },
+};
